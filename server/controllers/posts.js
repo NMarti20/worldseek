651b6eca780e6db34f26661c0a06b7f169bf82ab
NMarti20 <natasha.martinezsj@gmail.com>
2022-03-11 11:50:22 -0800

    created trycatch block for getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,18 @@
 //handles the logic
+import PostMessage from "../models/postsMessage.js";
 
-export const getPosts = (req, res) => {
-  res.send("Post Works!");
+export const getPosts = async (req, res) => {
+  try {
+    //try to retrieve all posts we have in database
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = (req, res) => {
+  res.send("Post Creation!");
 };
