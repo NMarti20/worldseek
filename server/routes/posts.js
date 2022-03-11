f940744aeb2eb8ae3014bcbbc9bec56e4703b58c
NMarti20 <natasha.martinezsj@gmail.com>
2022-03-11 11:40:37 -0800

    created a post req route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 // add all the routes that have to do anything with posts
 
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
