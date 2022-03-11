f84320e068b9a07e20e6e713f243d6e06a370d6e
NMarti20 <natasha.martinezsj@gmail.com>
2022-03-11 15:50:00 -0800

    imported posts and form components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import worldseek from "./images/worldseek.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="WorldSeek"></Typography>
+        <img src={worldseek} alt="worldseek" height="60"></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
