a2b623172af1fdfa7d0a0173ee0b34c77242868b
NMarti20 <natasha.martinezsj@gmail.com>
2022-03-15 13:08:04 -0700

    installed react-redux and fixed imports

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "react-thunk";
 import reducers from "./reducers/index";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
