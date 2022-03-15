bd1b743f4f0cdc54dd6ef283339d9ea9256ff011
NMarti20 <natasha.martinezsj@gmail.com>
2022-03-15 11:24:47 -0700

    connected stylesheet to js file

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Post from "./Post/Post";
+import usestyles from "./styles";
 
 const Posts = () => {
+  const classes = usestyles();
   return (
     <>
       <h1>Posts</h1>
